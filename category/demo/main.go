6de935ba6fe92987955fb031ae89892c6776a4c6
SmartWalle <smartwalle@gmail.com>
2018-03-12 15:31:41 +0800

    README.md

--- category/demo/main.go
+++ category/demo/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/smartwalle/dbs"
 	"github.com/smartwalle/m4go/category"
 	"fmt"
+	"database/sql"
 )
 
 func main() {
-	var pool = dbs.NewSQL("mysql", "", 10, 10)
+	var db, _ = sql.Open("mysql", "")
+	var cm = category.NewManager(db, "category")
 
-	var cm = category.NewManager(pool.GetSession(), "category")
-
-	categoryList, err := cm.GetCategoryList(34, 0, 0, 1, "", 0)
+	categoryList, err := cm.GetCategoryList(0, 0, 0, 1, "", 0)
 	if err != nil {
 		fmt.Println("err", err)
 		return
@@ -21,6 +20,4 @@ func main() {
 	for _, category := range categoryList {
 		fmt.Println(category.Type, category.Id, category.Name, category.Description, category.LeftValue, category.RightValue)
 	}
-
-	cm.UpdateCategoryParent(45, 37)
 }
