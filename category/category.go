4389c984aefa8172c0c33ef35cbedc25887a00af
Yangfeng <smartwalle@gmail.com>
2018-04-21 16:00:07 +0800

    优化

---
+++ category/category.go
@@ -0,0 +1,17 @@
+package category
+
+import (
+	"github.com/smartwalle/dbs"
+)
+
+type Manager struct {
+	db    dbs.DB
+	table string
+}
+
+func New(db dbs.DB, table string) *Manager {
+	var m = &Manager{}
+	m.db = db
+	m.table = table
+	return m
+}
